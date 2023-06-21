364bae0a6ba36fabf142c2190bc105b8792861bf
Vojan-Najov <serejah.mashichev@gmail.com>
2023-06-22 02:04:03 +0300

    Add s21_containersplus header.

---
+++ s21_containersplus.h
@@ -0,0 +1,8 @@
+// Copyright 2023 <Carmine Cartman, Vojan Najov>
+
+#ifndef S21_CONTAINERSPLUS_H_
+#define S21_CONTAINERSPLUS_H_
+
+#include "include/s21_array.h"
+
+#endif  // S21_CONTAINERSPLUS_H_
