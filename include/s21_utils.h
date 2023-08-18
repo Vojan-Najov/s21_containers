531d8b22fcc3bdb7853e260f9fe858b8f80d6650
Vojan-Najov <serejah.mashichev@gmail.com>
2023-08-18 20:24:58 +0300

    Check cpplint.

--- include/s21_utils.h
+++ include/s21_utils.h
@@ -1,7 +1,7 @@
 // Copyright 2023 <Carmine Cartman, Vojan Najov>
 
-#ifndef S21_UTILS_H_
-#define S21_UTILS_H_
+#ifndef INCLUDE_S21_UTILS_H_
+#define INCLUDE_S21_UTILS_H_
 
 namespace s21 {
 
@@ -9,43 +9,46 @@ namespace s21 {
 
 template <typename Arg, typename Result>
 struct UnaryFunction {
-	using argument_type = Arg;
-	using result_type = Result;
+  using argument_type = Arg;
+  using result_type = Result;
 };
 
 template <typename T>
 struct Identity : public UnaryFunction<T, T> {
-	T& operator()(T& x) const { return x; }
-	const T& operator()(const T& x) const { return x; }
+  T& operator()(T& x) const { return x; }
+  const T& operator()(const T& x) const { return x; }
 };
 
 template <typename Pair>
 struct Select1st : public UnaryFunction<Pair, typename Pair::first_type> {
-	typename Pair::first_type& operator()(Pair& x) const { return x.first; }
+  typename Pair::first_type& operator()(Pair& x) const { return x.first; }
 
-	const typename Pair::first_type& operator()(const Pair& x) const { return x.first; }
+  const typename Pair::first_type& operator()(const Pair& x) const {
+    return x.first;
+  }
 };
 
 template <typename Pair>
 struct Select2nd : public UnaryFunction<Pair, typename Pair::second_type> {
-	typename Pair::second_type& operator()(Pair& x) const { return x.second; }
+  typename Pair::second_type& operator()(Pair& x) const { return x.second; }
 
-	const typename Pair::second_type&
-	operator()(const Pair& x) const { return x.second; }
+  const typename Pair::second_type& operator()(const Pair& x) const {
+    return x.second;
+  }
 };
 
 template <typename Arg1, typename Arg2, typename Result>
 struct BinaryFunction {
-	using first_argument_type = Arg1;
-	using second_argument_type = Arg2;
-	using result_type = Result;
+  using first_argument_type = Arg1;
+  using second_argument_type = Arg2;
+  using result_type = Result;
 };
 
 template <typename T>
-struct Less: public BinaryFunction<T, T, bool> {
-	bool operator()(const T& lhs, const T& rhs) const { return lhs < rhs; }
+struct Less : public BinaryFunction<T, T, bool> {
+  bool operator()(const T& lhs, const T& rhs) const { return lhs < rhs; }
 };
 
-} // namespace s21
+}  // namespace s21
 
-#endif // S21_UTILS_H_
+#endif  // INCLUDE_S21_UTILS_H_
